5bc306eff531f4aa4a7502bd832ee3678cc79a83
Textspot <sachinkatarki1995@gmail.com>
2021-11-10 08:22:45 +0530

    Adde:Welcome to Line_comparision

--- src/com/bridgelab/line_comp/Linecomparision.java
+++ src/com/bridgelab/line_comp/Linecomparision.java
@@ -1,10 +1,29 @@
 package com.bridgelab.line_comp;
+import java.util.Scanner;
 
 public class Linecomparision {
 
 	public static void main(String[] args) {
+		 System.out.println("Welcome to Line comparison program !!");
+	        input();
+	    }
+
+	    public static void input() {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("enter the value of x = ");
+	        int x = sc.nextInt();
+	        System.out.print("enter the value of x1 = ");
+	        int x1 = sc.nextInt();
+	        System.out.print("enter the value of y = ");
+	        int y = sc.nextInt();
+	        System.out.print("enter the value of y1 = ");
+	        int y1 = sc.nextInt();
+
+	        double distance = Math.sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
+	        System.out.println("Distance = " + distance);
+	    }
 		
 
 	}
 
-}
+
